f732ed944dbd1fc70ff78ec1a39c01f216d11834
avgra3 <antonygradillas@gmail.com>
2025-03-24 13:50:08 -0700

    Added middleware

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
@@ -13,7 +15,14 @@ func main() {
 	// Handle that adds a file server.
 	// Fileserver uses the http.Dir to map the
 	// current directory to http address.
-	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{}
+	app := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInt(app))
+
+	// Handle hits to the file server
+	serverMux.HandleFunc("/metrics", apiCfg.hitCounter)
+	serverMux.HandleFunc("/reset", apiCfg.restCounter)
+
 	server := http.Server{
 		Handler: serverMux,
 		Addr:    ":8080",
@@ -23,6 +32,7 @@ func main() {
 
 }
 
+// Handlers
 // Want the readiness endpoint to be accessible at /healthz using any HTTP method
 // Should return: 200 OK status code
 // Header => Content-Type: text/plain; charset=utf-8
@@ -36,4 +46,47 @@ func readiness(respWriter http.ResponseWriter, req *http.Request) {
 	respWriter.WriteHeader(200)
 	// Write the body
 	respWriter.Write([]byte("OK"))
+}
+
+// Want a handler that writes the number of requests that have been counted as plain text in this format to the HTTP response:
+// Hits: x
+func (cfg *apiConfig) hitCounter(respWriter http.ResponseWriter, req *http.Request) {
+	// Set content-type
+	req.Header.Add("Content-Type", "text/plain; charset=utf-8")
+	// Write status code
+	respWriter.WriteHeader(200)
+	// Write the body
+	hitCounter := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	respWriter.Write([]byte(hitCounter))
+}
+
+// Want a handler to reset the counter
+func (cfg *apiConfig) restCounter(respWriter http.ResponseWriter, req *http.Request) {
+	// Set content-type
+	req.Header.Add("Content-Type", "text/plain; charset=utf-8")
+	// Write status code
+	respWriter.WriteHeader(200)
+	// Write the body
+	hitCounterBefore := fmt.Sprintf("Hits (before reset): %v", cfg.fileserverHits.Load())
+	// Resets back to zero
+	cfg.fileserverHits.Store(0)
+	hitCounterAfter := fmt.Sprintf("Hits (before reset): %v", cfg.fileserverHits.Load())
+	hitCounter := hitCounterBefore + "\n" + hitCounterAfter
+	respWriter.Write([]byte(hitCounter))
+}
+
+// Types
+type apiConfig struct {
+	// Allows us to safely increment an integer across goroutines
+	fileserverHits atomic.Int32
+}
+
+// Middleware
+func (cfg *apiConfig) middlewareMetricsInt(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increments the filerserverHits counter ever time it's called.
+		cfg.fileserverHits.Add(1)
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
 }
