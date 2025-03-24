81fc1ac987794b9b925b4a06560f05536f986075
avgra3 <antonygradillas@gmail.com>
2025-03-24 11:46:51 -0700

    Added handler for checking readiness

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	serverMux := http.NewServeMux()
+	// Setting up our readiness endpoint
+	serverMux.HandleFunc("/healthz", readiness)
+
 	// Handle that adds a file server.
 	// Fileserver uses the http.Dir to map the
 	// current directory to http address.
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	server := http.Server{
 		Handler: serverMux,
 		Addr:    ":8080",
@@ -18,4 +21,19 @@ func main() {
 	log.Printf("Running server on port: %v", server.Addr)
 	server.ListenAndServe()
 
+}
+
+// Want the readiness endpoint to be accessible at /healthz using any HTTP method
+// Should return: 200 OK status code
+// Header => Content-Type: text/plain; charset=utf-8
+// Body => OK
+// Later the endpoint can be enhanced to return 403 Service Unavailable status code
+// if the server is not ready
+func readiness(respWriter http.ResponseWriter, req *http.Request) {
+	// Set content-type
+	req.Header.Add("Content-Type", "text/plain; charset=utf-8")
+	// Write status code
+	respWriter.WriteHeader(200)
+	// Write the body
+	respWriter.Write([]byte("OK"))
 }
